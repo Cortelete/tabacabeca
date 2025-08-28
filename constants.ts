9ee45837c27419c9afaeb8d971ec9a5c19d71958
Davi Cortelete <111796499+Cortelete@users.noreply.github.com>
2025-08-28 12:38:26 -0300

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -2,6 +2,6 @@
 
 export const DEV_WHATSAPP_NUMBER = '5541988710303';
 export const GOOGLE_MAPS_URL = 'https://maps.app.goo.gl/LTeVX85WcPAhWUUr7';
-export const GOOGLE_REVIEW_URL = 'https://www.google.com/search?q=tabacabe%C3%A7a&newwindow=1&sca_esv=49d605c33f00d0c3&sxsrf=AE3TifP29rD73jRrTqTjPe0S0ksLX23F5w%3A1756229638524&source=hp&ei=BvCtaOejHY2_5OUP6N_iuAE&iflsig=AOw8s4IAAAAAaK3-Frb3SXlmG6igJKH-RUfGe5GycskL&ved=0ahUKEwjnvvudgamPAxWNH7kGHeivGBcQ4dUDCBc&uact=5&oq=tabacabe%C3%A7a&gs_lp=Egdnd3Mtd2l6Igt0YWJhY2FiZcOnYTIOEC4YgAQYxwEYjgUYrwEyAhAmMgUQABjvBTIFEAAY7wUyCBAAGIAEGKIEMggQABiABBiiBEjRFlAAWLEVcAF4AJABAJgBlwGgAaICqgEDMC4yuAEDyAEA-AEC-AEBmAICoAKdAZgDAJIHAzEuMaAHmAmyBwMwLjG4B5kBwgcFMC4xLjHIBwU&sclient=gws-wiz#lrd=0x94e81b2f08617ed1:0x7eb4a3676b060224,3,,,,';
+export const GOOGLE_REVIEW_URL = 'https://search.google.com/local/writereview?placeid=ChIJ0X5hCC8b6JQRJAIGa2ejtH4';
 // IMPORTANT: Replace with your actual email for FormSubmit to work.
 export const FORM_SUBMIT_EMAIL = 'your@email.com';
